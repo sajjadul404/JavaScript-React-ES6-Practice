524082f7c864869dcab5f57809ef49e7a72dfce5
sajjadul404 <sajjadul625@gmail.com>
2026-02-24 01:37:23 +0600

    Filter expenses > 1000

--- phase-1/day-3/day3.js
+++ phase-1/day-3/day3.js
@@ -16,3 +16,11 @@ const totalExpense = expenses.reduce((total, expense) => {
 
 console.log("Total Expense:", totalExpense);
 
+//Filter expenses > 1000
+const highExpenses = expenses.filter(expense => expense.amount > 1000);
+
+console.log("\nExpenses greater than 1000:");
+highExpenses.forEach(expense => {
+  console.log(`${expense.title}: ${expense.amount}`);
+});
+
