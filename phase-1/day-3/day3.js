ebbe0ab25e503af2a15d66fe78075a10a9b62576
sajjadul404 <sajjadul625@gmail.com>
2026-02-24 01:34:22 +0600

    Expense Array

---
+++ phase-1/day-3/day3.js
@@ -0,0 +1,10 @@
+//Expense Array
+const expenses = [
+  { title: "Groceries", amount: 1200 },
+  { title: "Electric Bill", amount: 800 },
+  { title: "Internet", amount: 1500 },
+  { title: "Transport", amount: 600 },
+  { title: "Shopping", amount: 2000 }
+];
+
+console.log("===== EXPENSE SUMMARY =====");
