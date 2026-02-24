27b2dddbfd0d1f46b10e6c3a2f234618c4084d9a
sajjadul404 <sajjadul625@gmail.com>
2026-02-25 02:18:26 +0600

    Add Methods

--- phase-1/day-4/day4.js
+++ phase-1/day-4/day4.js
@@ -1,5 +1,28 @@
 const user = {
   name: "Sajjad",
   age: 22,
-  skills: []
+  skills: [],
+
+  addSkill(skill) {
+    if (!this.skills.includes(skill)) {
+      this.skills.push(skill);
+      console.log(`${skill} added.`);
+    } else {
+      console.log(`${skill} already exists.`);
+    }
+  },
+
+  removeSkill(skill) {
+    this.skills = this.skills.filter(s => s !== skill);
+    console.log(`${skill} removed.`);
+  },
+
+  updateAge(newAge) {
+    if (newAge > 0) {
+      this.age = newAge;
+      console.log(`Age updated to ${newAge}`);
+    } else {
+      console.log("Invalid age");
+    }
+  }
 };
