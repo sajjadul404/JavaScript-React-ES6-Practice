3339084e36356974be09b3aaa0e8d2207d222819
sajjadul404 <sajjadul625@gmail.com>
2026-03-09 16:19:46 +0600

    pages

---
+++ phase-3/day-13/react-app/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <h2>Welcome to Home Page</h2>
+    </div>
+  );
+}
+
+export default Home;
